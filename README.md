ec7500aac0594e1f2c1f58d92cfcc480597ccdde
luckyJim-dev <gispro@gmail.com>
2019-12-15 14:48:47 +0800

    get POI using baidu api

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# baidu_poi
+# 1. 所需安装包
+`pip install mapapi`
+`pip install pykml`
+`pip install xpinyin`
+# 2. 主要参考资料
+## 2.1 mapapi
+百度地图Web API接口封装，可以使用mapapi模块，具体源码地址如下：
+`https://github.com/yimian/mapapi`
+## 2.2 pyKML
+pyKML是一个Python包，用于创建，解析，操作和验证KML（一种用于对地理数据进行编码和注释的语言）。
+`https://pythonhosted.org/pykml/`
+## 2.3 xpinyin
+快速把汉字转换为拼音
+## 2.4 Coord_Transform
+用于百度坐标系、谷歌高德坐标系、WGS84坐标系之间的相互转换
+`https://github.com/tianyuningmou/Coord_Transform`
---
+++ get_poi.py
@@ -0,0 +1,59 @@
+# -*- coding: UTF-8 -*-
+
+from mapapi import baidu
+import json
+import logging
+import re
+from xpinyin import Pinyin
+import lnglatTransfer as Transfer
+from pykml.factory import KML_ElementMaker as KML
+from lxml import etree
+
+def get_file_name_by_city(city):
+    piny = Pinyin()
+    cittyName = piny.get_pinyin(city,'')
+    return ('data/{0}.csv'.format(cittyName))
+
+def get_kml_file_by_city(city):git commit -m "first commit"
+    piny = Pinyin()
+    cittyName = piny.get_pinyin(city,'')
+    return ('data/{0}.kml'.format(cittyName))
+
+def get_tags_by_city(tags,city):
+    map_api = baidu.MapApi()
+    file_name = get_file_name_by_city(city)
+    kml_name = get_kml_file_by_city(city)
+    fold = KML.Folder()
+    with open(kml_name,'w') as kml_f:
+        with open(file_name,'w') as f:
+            print("Start...")
+            f.write("{},{},{}".format('name','lat','lng'))
+            for tag in tags:
+                data = map_api.place_api.get_place_all(tag,city)
+                for item in data:
+                    try:
+                        name = item['name']
+                        lng = float(item['location']['lng'])
+                        lat = float(item['location']['lat'])
+                        [g_lng,g_lat] = Transfer.bd09_to_wgs84(lng,lat)
+                        # Write data in CSV
+                        f.write("{},{},{}".format(name,g_lng,g_lat))
+                        # Write data in kml
+                        fold.append(
+                            KML.Placemark(
+                                KML.name(name),
+                                KML.Point(KML.coordinates(str(g_lng)+","+str(g_lat)))
+                            ))
+                    except:
+                        logging.exception('Error: Parsing json file ')
+            f.close()
+        output = etree.tostring(fold,pretty_print=True)
+        kml_f.write(output.decode('utf-8'))
+        kml_f.close()
+    print("sucess")
+    pass  
+
+if __name__ == '__main__':
+    tags = ['高等院校']
+    city = "西安"
+    get_tags_by_city(tags,city)
